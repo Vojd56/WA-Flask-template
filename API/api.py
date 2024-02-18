c2a9854ba92840cc2c22e630ce9828b46a0fe2a3
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-02-18 22:32:29 +0100

    create get all products

--- API/api.py
+++ API/api.py
@@ -1,13 +1,20 @@
 from flask import Blueprint, render_template
 import requests
+import json
 api_bp = Blueprint('api_bp', __name__,
     template_folder='templates',
     static_folder='static')
+
 URL_API = "https://fakestoreapi.com"
 
 def ReadAllProducts():
-    return requests.get(f"{URL_API}/products")
+     
+    request = requests.get(f"{URL_API}/products")
     
-#@api_bp.route('/')
-#def index():
-#    return render_template('zatím nic')
+    result = json.loads(request.text)
+    return result
+
+#def GetAllProducts():
+#    return json.load(ReadAllProducts())
+    
+
--- products/products.py
+++ products/products.py
@@ -1,11 +1,12 @@
 from flask import Blueprint, render_template
-from API.api import ReadAllProducts
+from API.api import *
+
 products_bp = Blueprint('products_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
 @products_bp.route('/products')
 def index():
-    response = ReadAllProducts().json()
-    
-    return render_template('products/products.html', response=response)
+    r = ReadAllProducts()
+    l = len(r)
+    return render_template('products/products.html',length = l, products = r)
